03cd0ea27378cb8bb24197063b695767610b331f
André Lopes <andrellopes021@gmail.com>
2023-01-07 18:33:31 -0300

    clear tasks function

--- src/Layout/Main.tsx
+++ src/Layout/Main.tsx
@@ -1,4 +1,15 @@
-import { Center, Container, Flex, SimpleGrid, Spacer } from '@chakra-ui/react'
+import {
+    Button, Center, Container, Flex, SimpleGrid, Spacer,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverFooter,
+    PopoverArrow,
+    PopoverCloseButton,
+    PopoverAnchor,
+} from '@chakra-ui/react'
 import React, { useContext } from 'react'
 import TaskCard from '../Components/TaskCard'
 import { TasksContext } from '../Contexts/TaskContext'
@@ -8,21 +19,41 @@ type Props = {}
 
 const Main = (props: Props) => {
 
-    const { currentTasks } = useContext(TasksContext) as TaskContextType
+    const { currentTasks, clearTasks } = useContext(TasksContext) as TaskContextType
 
     return (
         <Container maxW='none' w='100%' bgColor='layoutBg' m='20px' borderRadius='20px'>
+            <Flex w='100%' justifyContent='flex-end' my='20px'>
+                {!!currentTasks.length &&
+                    <Popover placement='left-start'>
+                        <PopoverTrigger>
+                            <Button bgColor='compBg' color='txtColor' _hover={{ color: 'compBg', bgColor: 'txtColor' }}>Clear tasks</Button>
+                        </PopoverTrigger>
+                        <PopoverContent bgColor='compBg' color='txtColor' border='none'>
+                            <PopoverArrow />
+                            <PopoverCloseButton />
+                            <PopoverHeader>Confirm to clear all the Tasks</PopoverHeader>
+                            <PopoverBody>
+                                <Button bgColor='txtColor' color='compBg' w='100%'
+                                    _hover={{ color: 'txtColor', bgColor: 'compBg' }}
+                                    onClick={clearTasks} >Clear tasks</Button>
+                            </PopoverBody>
+                        </PopoverContent>
+                    </Popover>
+                }
 
-                <SimpleGrid  minChildWidth='200px' spacing={8} p='30px'>
-                    {!currentTasks.length && <h1> no tasks</h1>}
-                    {currentTasks &&
-                        currentTasks.map((task: ITask) => {
-                            return (
-                                <TaskCard task={task} key={task.id} />
-                            )
-                        })
-                    }
-                </SimpleGrid>
+
+            </Flex>
+            <SimpleGrid minChildWidth='200px' spacing={8} p='30px'>
+                {!currentTasks.length && <h1> no tasks</h1>}
+                {currentTasks &&
+                    currentTasks.map((task: ITask) => {
+                        return (
+                            <TaskCard task={task} key={task.id} />
+                        )
+                    })
+                }
+            </SimpleGrid>
         </Container>
     )
 }
